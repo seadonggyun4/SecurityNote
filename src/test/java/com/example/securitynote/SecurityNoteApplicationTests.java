74cb44a5661a1d9f2990fa7ff939dcba0972db16
seadonggyun4 <seadonggyun@gmail.com>
2023-11-17 17:57:52 +0900

    SecurityNote Project start

---
+++ src/test/java/com/example/securitynote/SecurityNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
